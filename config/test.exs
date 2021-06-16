31e2e1dd3406193db3c0c4768fd871765171fd15
Dan Hunsaker <danhunsaker@gmail.com>
2021-06-16 16:48:38 -0600

    fix config for portability

--- config/test.exs
+++ config/test.exs
@@ -5,7 +5,7 @@ config :qwit, QWIT.Oban.TestCase.Repo,
   username: System.get_env("DATA_DB_USER") || "postgres",
   password: System.get_env("DATA_DB_PASS") || "postgres",
   hostname: System.get_env("DATA_DB_HOST") || "localhost",
-  database: "gonano",
+  database: (if System.get_env("APP_IP"), do: "gonano", else: "qwit"),
   pool: Ecto.Adapters.SQL.Sandbox
 
 # Print only warnings and errors during test
